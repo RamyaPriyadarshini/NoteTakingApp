005d808b342177fcfab423b3244955587122703b
Ramya <ramyaphone78@gmail.com>
2019-07-08 20:44:32 +0530

    CSS for layout

--- notes/src/components/App.js
+++ notes/src/components/App.js
@@ -4,13 +4,14 @@ import NoteNav from './NoteNav/NoteNav';
 import Editor from './Editor/Editor';
 class App extends Component {
   render() {
+    const styles = {height: '100%',margin:"0px"};
     return (
-      <div className="App">
-        <NotebookNav/>
-        <br/>
-        <NoteNav/>
-        <br/>
-        <Editor/>
+      <div style={styles} className="App">
+        <div className="row" style={styles}>
+          <div style={styles} className="col s2 teal darken-2"><NotebookNav/></div>
+          <div style={styles} className="col s2 teal"><NoteNav/></div>
+          <div style={styles} className="col s8 grey lighten-3"><Editor/></div>
+        </div>
       </div>
     );
   }
